783aaf8a24f7f285491eaa77adae1efb456dd434
elikibula <elikibula@gmail.com>
2025-06-23 21:42:32 +0000

    Your meaningful commit message

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Device
 
@@ -8,15 +7,3 @@ class DeviceAdmin(admin.ModelAdmin):
     list_filter = ('device_type', 'status', 'date_received')
     search_fields = ('name', 'contact', 'brand', 'model')
     ordering = ('-date_received',)
-=======
-from django.contrib import admin
-from .models import Device
-
-@admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'contact', 'device_type', 'status')
-    list_filter = ('device_type', 'status', 'date_received')
-    search_fields = ('name', 'contact', 'brand', 'model')
-    ordering = ('-date_received',)
- 
->>>>>>> c500efc63b6eb4ad91a328a9faa3903e2a7ef3c6
